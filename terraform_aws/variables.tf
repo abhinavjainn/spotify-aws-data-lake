bbd91c7ca242578a897cd9c95f86602c14d43402
ABHINAV JAIN <jain.abhinav85@gmail.com>
2022-03-16 01:23:32 +0000

    remove dead code

--- terraform_aws/variables.tf
+++ terraform_aws/variables.tf
@@ -5,15 +5,3 @@ variable "TF_VAR_SPOTIPY_CLIENT_ID" {
 variable "TF_VAR_SPOTIPY_CLIENT_SECRET" {
   type = string
 }
-
-#variable "TF_VAR_ACCESS_KEY" {
-#  type = "string"
-#}
-#
-#variable "TF_VAR_SECRET_KEY" {
-#  type = "string"
-#}
-
-#variable "region" {
-#  default = "eu-west-2"
-#}
